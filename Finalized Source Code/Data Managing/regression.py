beccb06ee5260c97dbea409306cfe5d06547bbeb
dlike230 <dlike230@gmail.com>
2018-07-17 14:33:02 -0400

    Changes
    Changed regression to ElasticNet. Wrote code to create distribution of
    losses with fabricated data.

--- Finalized Source Code/Data Managing/regression.py
+++ Finalized Source Code/Data Managing/regression.py
@@ -2,6 +2,10 @@ import numpy as np
 from sklearn.linear_model import Ridge
 import matplotlib.pyplot as plt
 import random
+import seaborn as sns
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import SGDRegressor
+from sklearn.linear_model import ElasticNet
 
 def extract_data_from_csv(root):
     np_arr = np.genfromtxt(root+'combined.csv', delimiter=',')
@@ -36,7 +40,7 @@ def generatePredictions(clf,testing_vectors,n):
                 vector.append(prediction)
     return predictions
 
-def calculate_cost(clf,predictions,testing_labels):
+def calculate_cost(predictions,testing_labels):
     total = 0
     count = 0
     for prediction,label in zip(predictions,testing_labels):
@@ -59,10 +63,6 @@ def plot(data_1, data_2):
     plt.subplot(212)
     plt.plot(data_2)
     plt.show()
-
-data = extract_data_from_csv("")
-
-sample_size = 10000
 
 def separateData(data,trainingSize,trainingStart,testingEnd):
     gap_between_training_testing = int(len(data) * 0.1)
@@ -87,36 +87,62 @@ def generateAllData(data,trainingSize,trainingStart,testingEnd,n,falsifyTesting)
     
 
 def getTrainedModel(vectors, labels):
-    clf = Ridge()
+    #clf = Ridge()
+    clf = ElasticNet()
     clf.fit(vectors,labels)
     return clf
 
-def computeCost(n,falsify):
-    print("Gathering data")
-    raw_data = extract_data_from_csv("")
-    training_vectors,training_labels,testing_vectors,testing_labels = generateAllData(raw_data,0.8,0,1,n,falsify)
-    print("Finished gathering data")
-    
-    print("training model")
-    clf = getTrainedModel(training_vectors,training_labels)
-    print("model trained")
-
+def computeCost(n,falsify,trained_model,testing_data):
+    if(falsify):
+        testing_data = generate_fake(testing_data,1)
+    testing_vectors, testing_labels = generateVectors(n,testing_data)
     print("generating predictions")
-    predictions = generatePredictions(clf,testing_vectors,n)
+    predictions = generatePredictions(trained_model,testing_vectors,n)
     print("predictions generated")
-
-    cost = calculate_cost(clf,predictions,testing_labels)
+    cost = calculate_cost(predictions,testing_labels)
     print("Cost: "+str(cost))
     return cost,testing_labels,predictions
 
-def main():
-    cost_normal,testing_labels,predictions = computeCost(10,False)
-    cost_falsified,testing_labels,predictions_falsified = computeCost(10,True)
+def quickComparison():
+    print("Gathering data")
+    raw_data = extract_data_from_csv("")
+    training_data, testing_data = separateData(raw_data,0.75,0,1)
+    training_vectors,training_labels = generateVectors(10,training_data)
+    print("Finished gathering data")    
+    print("training model")
+    clf = getTrainedModel(training_vectors,training_labels)
+    print("model trained")
+    cost_normal,testing_labels,predictions = computeCost(10,False,clf,testing_data)
+    cost_falsified,testing_labels,predictions_falsified = computeCost(10,True,clf,testing_data)
     print("Cost when data is not falsified: "+str(cost_normal))
     print("Cost when data is falsified: "+str(cost_falsified))
     plot(predictions,predictions_falsified)    
 
+def generateCostDistributionWhenFalsified(n,size):
+    print("Gathering data")
+    raw_data = extract_data_from_csv("")
+    training_data, testing_data = separateData(raw_data,0.75,0,1)
+    training_vectors,training_labels = generateVectors(10,training_data)  
+    print("Finished gathering data")    
+    print("training model")
+    clf = getTrainedModel(training_vectors,training_labels)
+    print("model trained")
+    normal_cost = computeCost(n,False,clf,testing_data)[0]
+    costs = list()
+    for i in range(size):
+        costs.append(computeCost(n,True,clf,testing_data)[0])
+    return costs,normal_cost
+
+def main():
+    costs,normal_cost = generateCostDistributionWhenFalsified(10,250)
+    print("Cost when unmodified is: "+str(normal_cost))
+    print("Graphing distribution of cost when modified...")
+    sns.set(color_codes=True)
+    sns.distplot(costs)
+    plt.show()
+
 main()
+    
 
 
     
