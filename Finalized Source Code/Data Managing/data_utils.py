f0620cc3e230d425ebc2c856235d8538547015f6
dlike230 <dlike230@gmail.com>
2018-07-19 14:51:45 -0400

    Restructured code
    Added data_utils.py to allow reusal of certain functions by multiple
    python scripts

---
+++ Finalized Source Code/Data Managing/data_utils.py
@@ -0,0 +1,15 @@
+import numpy as np
+import math
+#takes the root directory for combined.csv as an argument, returns a tuple of corresponding values for each data entry
+def extract_data_from_csv(root):
+    np_arr = np.genfromtxt(root+'combined.csv', delimiter=',')
+    all_data = list()
+    for time,AT,OT,RH,Bar,Light in np_arr:
+        if valid_data(AT) and valid_data(OT) and valid_data(RH) and valid_data(Bar) and valid_data(Light):
+            all_data.append((AT,OT,RH,Bar,Light))
+    #all_data = all_data[::30]
+    return all_data
+
+#returns true if the data is a valid number
+def valid_data(item):
+    return item is not None and not math.isnan(item)
--- Finalized Source Code/Data Managing/regression.py
+++ Finalized Source Code/Data Managing/regression.py
@@ -7,20 +7,8 @@ from sklearn.linear_model import Lasso
 from sklearn.linear_model import SGDRegressor
 from sklearn.linear_model import ElasticNet
 import math
-
-#returns true if the data is a valid number
-def valid_data(item):
-    return item is not None and not math.isnan(item)
-
-#takes the root directory for combined.csv as an argument, returns a tuple of corresponding values for each data entry
-def extract_data_from_csv(root):
-    np_arr = np.genfromtxt(root+'combined.csv', delimiter=',')
-    all_data = list()
-    for time,AT,OT,RH,Bar,Light in np_arr:
-        if valid_data(AT) and valid_data(OT) and valid_data(RH) and valid_data(Bar) and valid_data(Light):
-            all_data.append((AT,OT,RH,Bar,Light))
-    #all_data = all_data[::30]
-    return all_data
+from data_utils import extract_data_from_csv
+from data_utils import valid_data
 
 # turns a list of tuples or lists into a 1D list
 def flatten(arr):
