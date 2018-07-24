8745437d7b565097b11c09d7512e19bafc3bcc36
dlike230 <dlike230@gmail.com>
2018-07-24 10:46:49 -0400

    Added methods to query data based on dates
    added getDate() method and generateDateDictionaryMethod()

--- Finalized Source Code/Data Managing/data_utils.py
+++ Finalized Source Code/Data Managing/data_utils.py
@@ -18,3 +18,21 @@ def extract_data_from_csv(root):
 #returns true if the data is a valid number
 def valid_data(item):
     return item is not None and not math.isnan(item)
+
+#gets the date for a specific index in the data
+def getDate(index,root):
+    i = -1
+    for line in open(root+'combined.csv'):
+        if(i == index):
+            return line.split(",")[0]
+        i += 1
+
+#returns a dictionary that allows you to find data for a specific date
+def generateDateDictionary(root):
+    data = list()
+    for line in open(root+'combined.csv'):
+        data.append(line.split(","))
+    dictionary = dict()
+    for sub_list in data:
+        dictionary[sub_list[0]] = sub_list[1:]
+    return dictionary
