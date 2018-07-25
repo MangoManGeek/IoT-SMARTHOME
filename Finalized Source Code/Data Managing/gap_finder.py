b467535316f4a13c6d9677d1cd3021cc5f47867d
dlike230 <dlike230@gmail.com>
2018-07-25 14:32:11 -0400

    Updated gap detection

--- Finalized Source Code/Data Managing/gap_finder.py
+++ Finalized Source Code/Data Managing/gap_finder.py
@@ -14,15 +14,22 @@ def checkForGapsAfter(lastCheckedDate,root, min_size_seconds):
     last_checked_date_as_seconds = 0 if lastCheckedDate is None else datetime_object_to_seconds(datetime_object_from_text(lastCheckedDate))
     all_dates = getDates(root)
     searchingForStartPoint = True
+    log_file_output = ""
     lastDate = None
     for date_string, date_seconds in all_dates:
         if(searchingForStartPoint and date_seconds >= last_checked_date_as_seconds):
             searchingForStartPoint = False
-        if(lastDate is not None and date_seconds - lastDate > min_size_seconds):
-            print("GAP FOUND AT " + date_string)
+        if(lastDate is not None):
+            gap_size = date_seconds - lastDate
+            if(gap_size> min_size_seconds):
+                if(len(log_file_output) > 0):
+                    log_file_output += "\n"
+                log_file_output += str(int(gap_size))+"-SECOND GAP FOUND AT " + date_string
         lastDate = date_seconds
+    open("gap_log.txt","w").write(log_file_output)
+    print("Wrote to gap_log.txt")
 
 
-checkForGapsAfter(None,"",120)
+checkForGapsAfter(None,"",90)
         
         
